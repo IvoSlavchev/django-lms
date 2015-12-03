14f6697df74b6a0512fc59d2cddd794085e78f82
IvoSlavchev <ivelin.slavchev97@gmail.com>
2015-12-03 22:05:08 +0200

    Adding UserProfile and an empty /confirm in urls

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class User(AbstractBaseUser):
 	username = models.CharField(max_length=30, unique=True, db_index=True)
 	email = models.EmailField(max_length=30)
 	is_teacher = models.BooleanField(default=False)
-	date_joined = models.DateTimeField(default=datetime.now, blank=True)
+	date_joined = models.DateTimeField(default=datetime.now)
 	is_active = models.BooleanField(default=True)
 
 	USERNAME_FIELD = 'username'
@@ -16,3 +16,9 @@ class User(AbstractBaseUser):
 
 	def __unicode__(self):
 		return self.username
+
+class UserProfile(models.Model):
+
+	user = models.OneToOneField(User)
+	activation_key = models.CharField(max_length=40, default='')
+	key_expires = models.DateTimeField(default=datetime.now)
--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^teacher', views.teacher_page, name='teacher'),
     url(r'^student', views.student_page, name='student'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^confirm/$', views.confirm, name='confirm'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
 ]
