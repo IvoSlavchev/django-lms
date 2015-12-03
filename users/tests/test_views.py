a1a9563684da2b05934fe273d36b2c1ab8f9dd1d
IvoSlavchev <ivelin.slavchev97@gmail.com>
2015-12-03 22:46:07 +0200

    Adding tests for views

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -2,7 +2,8 @@ from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from users.views import home_page, login, signup
+from users.views import home_page, teacher_page, student_page, login, signup, confirm
+from django.http import Http404
 from users.models import User
 User = User()
 
@@ -18,14 +19,31 @@ class HomePageTest(TestCase):
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
 
+class TeacherAndStudentPageTest(TestCase):
+
+	def test_pages_raise_error_on_anonymous_user(self):
+		request = HttpRequest()	
+		with self.assertRaises(AttributeError):
+			response_teacher = teacher_page(request)
+
+		request = HttpRequest()
+		with self.assertRaises(AttributeError):
+			response_student = student_page(request)
+
 class LoginAndSignup(TestCase):
 
-	def test_pages_return_correct_html(self):
+	def test_signup_and_login_use_correct_template(self):
+		request = HttpRequest()
+		response_signup = signup(request)
+		with self.assertTemplateUsed('signup.html'):
+			render_to_string('signup.html')
+
 		request = HttpRequest()
 		response_login = login(request)
-		expected_html = render_to_string('login.html')
-		self.assertEqual(response_login.content.decode(), expected_html)
+		with self.assertTemplateUsed('login.html'):
+			render_to_string('login.html')
 
-		response_signup = signup(request)
-		expected_html = render_to_string('signup.html')
-		self.assertEqual(response_login.content.decode(), expected_html)
+	def test_confirm_raises_404_error(self):
+		request = HttpRequest()
+		with self.assertRaises(Http404):
+			response_confirm = confirm(request)
