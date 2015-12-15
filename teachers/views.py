e3af4b280afe91171b8d78f6fa9b882181380214
IvoSlavchev <ivelin.slavchev97@gmail.com>
2015-12-15 23:23:39 +0200

    Adding all student account list on teacher dashboard

--- teachers/views.py
+++ teachers/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 
 from teachers.forms import CreateForm
 from teachers.models import Course
+from users.models import User
 
 def teacher_check(user):
     return user.is_teacher
@@ -12,7 +13,9 @@ def teacher_check(user):
 @user_passes_test(teacher_check, login_url='/login')
 def dashboard(request):
 	courses = Course.objects.filter(owner=request.user)
-	return render_to_response('teacher_dashboard.html', {'courses': courses}, context_instance=RequestContext(request))
+	students = User.objects.filter(is_teacher=False)
+	return render_to_response('teacher_dashboard.html', {'courses': courses, 'students': students}, 
+		context_instance=RequestContext(request))
 
 def create(request):
 	if request.method == 'POST':		
