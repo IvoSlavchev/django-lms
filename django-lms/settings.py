5e66ee1d7a891fd8ce9c8113c270a4ccd0eb2438
IvoSlavchev <ivelin.slavchev97@gmail.com>
2015-12-15 23:15:21 +0200

    Extracting base.html in a global template folder, removing unused htmls

--- django-lms/settings.py
+++ django-lms/settings.py
@@ -48,7 +48,10 @@ ROOT_URLCONF = 'django-lms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(__file__), 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(os.path.dirname(__file__), 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
