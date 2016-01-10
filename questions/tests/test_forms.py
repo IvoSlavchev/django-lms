b43f69b5abc6c4121f37def5d956f09d6af4157c
IvoSlavchev <ivelin.slavchev97@gmail.com>
2016-01-10 17:44:31 +0200

    Adding the migration and a basic unit test for question form

--- questions/tests/test_forms.py
+++ questions/tests/test_forms.py
@@ -6,18 +6,26 @@ from questions.forms import QuestionForm, ChoiceForm
 class QuestionFormTest(TestCase):
 
 	def test_if_valid_on_all_required_fields(self):
-		form_data = {'name': 'Example name',
+		form_data = {'name': 'Example name', 'category': 'Examples',
 			'question_text': 'Example question text'}
 		form = QuestionForm(data=form_data)
 		self.assertTrue(form.is_valid())
 
 	def test_if_not_valid_on_empty_name(self):
-		form_data = {'name': '', 'question_text': 'Example question text'}
+		form_data = {'name': '', 'category': 'Examples',
+			'question_text': 'Example question text'}
+		form = QuestionForm(data=form_data)
+		self.assertFalse(form.is_valid())	
+
+	def test_if_not_valid_on_empty_category(self):
+		form_data = {'name': 'Example name', 'category': '',
+			'question_text': 'Example question text'}
 		form = QuestionForm(data=form_data)
 		self.assertFalse(form.is_valid())	
 
 	def test_if_not_valid_on_empty_question_text(self):
-		form_data = {'name': 'Example name', 'question_text': ''}
+		form_data = {'name': 'Example name', 'category': 'Examples',
+			'question_text': ''}
 		form = QuestionForm(data=form_data)
 		self.assertFalse(form.is_valid())
 
