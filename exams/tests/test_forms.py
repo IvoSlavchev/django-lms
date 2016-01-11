a182b88a3d56f8008af68bf258bf97474500c7ca
IvoSlavchev <ivelin.slavchev97@gmail.com>
2016-01-12 00:28:05 +0200

    Changing exams unit tests to account for changed categories

--- exams/tests/test_forms.py
+++ exams/tests/test_forms.py
@@ -1,48 +1,65 @@
 from django.test import TestCase
 
+from courses.models import Course
 from exams.forms import ExamForm
+from questions.models import Question
 
 
 class ExamFormTest(TestCase):
 
     def test_if_valid_on_all_fields(self):
+        course = Course.objects.create(name='Example name')
+        question = Question.objects.create(name='Example name',
+            category='Examples', question_text='Example', course=course)
         form_data = {'name': 'Example name',
             'description': 'Example description', 'password': 'example',
             'time_limit': '00:10', 'active_from': '01/01/2016 10:00',
             'active_to': '10/10/2016 22:00', 'category': 'Examples', 
             'question_count': '2'}
-        form = ExamForm(data=form_data)
+        form = ExamForm(data=form_data, course=course)
         self.assertTrue(form.is_valid())
 
     def test_if_valid_on_empty_description_and_password(self):
+        course = Course.objects.create(name='Example name')
+        question = Question.objects.create(name='Example name',
+            category='Examples', question_text='Example', course=course)
         form_data = {'name': 'Example name', 'description': '',
             'password': '', 'time_limit': '00:10',
             'active_from': '01/01/2016 10:00',
             'active_to': '10/10/2016 22:00', 'category': 'Examples', 
             'question_count': '2'}
-        form = ExamForm(data=form_data)
+        form = ExamForm(data=form_data, course=course)
         self.assertTrue(form.is_valid())
 
     def test_if_not_valid_on_empty_name(self):
+        course = Course.objects.create(name='Example name')
+        question = Question.objects.create(name='Example name',
+            category='Examples', question_text='Example', course=course)
         form_data = {'name': '', 'description': '', 'password': 'example',
             'time_limit': '00:10', 'active_from': '01/01/2016 10:00',
             'active_to': '10/10/2016 22:00', 'category': 'Examples', 
             'question_count': '2'}
-        form = ExamForm(data=form_data)
+        form = ExamForm(data=form_data, course=course)
         self.assertFalse(form.is_valid())
 
     def test_if_not_valid_on_empty_limit_and_dates(self):
+        course = Course.objects.create(name='Example name')
+        question = Question.objects.create(name='Example name',
+            category='Examples', question_text='Example', course=course)
         form_data = {'name': 'Example name', 'description': '',
             'password': 'example', 'time_limit': '', 'active_from': '',
             'active_to': '', 'category': 'Examples', 
             'question_count': '2'}
-        form = ExamForm(data=form_data)
+        form = ExamForm(data=form_data, course=course)
         self.assertFalse(form.is_valid())
 
     def test_if_not_valid_on_empty_category_and_questions(self):
+        course = Course.objects.create(name='Example name')
+        question = Question.objects.create(name='Example name',
+            category='Examples', question_text='Example', course=course)
         form_data = {'name': 'Example name', 'description': '',
             'password': 'example', 'time_limit': '00:10',
             'active_from': '01/01/2016 10:00', 'active_to': '10/10/2016 22:00',
             'category': '', 'question_count': ''}
-        form = ExamForm(data=form_data)
+        form = ExamForm(data=form_data, course=course)
         self.assertFalse(form.is_valid())
