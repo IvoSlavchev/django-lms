dbf236ea5285c78b39a2487a22e32fd58b176d58
IvoSlavchev <ivelin.slavchev97@gmail.com>
2016-01-14 00:03:09 +0200

    Adding users views and backends tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from users.models import User
 from users.forms import AuthenticationForm, RegistrationForm
 from users.views import home_page, login, signup, confirm
 
@@ -21,6 +22,14 @@ class HomePageTest(TestCase):
 
 class SignupTest(TestCase):
 
+    def test_signup_response(self):
+        response = self.client.get('/signup')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Sign up')
+        response = self.client.post('/signup', {'username': 'test',
+            'password': 'test'})
+        self.assertEqual(response.status_code, 200)
+
     def test_url_resolves_to_signup_page(self):
         found = resolve('/signup')
         self.assertEqual(found.func, signup)
@@ -37,6 +46,14 @@ class SignupTest(TestCase):
 
 class LoginTest(TestCase):
 
+    def test_login_response(self):
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Log in')
+        response = self.client.post('/login', {'username': 'test',
+            'password': 'test'})
+        self.assertEqual(response.status_code, 200)
+
     def test_url_resolves_to_login_page(self):
         found = resolve('/login')
         self.assertEqual(found.func, login)
@@ -49,3 +66,10 @@ class LoginTest(TestCase):
     def test_login_authentication_form(self):
         response = self.client.get('/login')
         self.assertIsInstance(response.context['form'], AuthenticationForm)
+
+
+class ConfirmTest(TestCase):
+
+    def test_confirm_response_fails(self):
+        response = self.client.get('/confirm?q=example')
+        self.assertEqual(response.status_code, 404)
