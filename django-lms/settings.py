fc508a953a02bf8d1cd94f17b30394a332cf7c2e
IvoSlavchev <ivelin.slavchev97@gmail.com>
2016-02-18 17:22:24 +0200

    Adding password change functionality

--- django-lms/settings.py
+++ django-lms/settings.py
@@ -51,6 +51,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'users/templates'),
             os.path.join(os.path.dirname(__file__), 'templates')
         ],
         'APP_DIRS': True,
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from users import views
 
@@ -8,4 +9,7 @@ urlpatterns = [
     url(r'^signup$', views.signup, name='signup'),
     url(r'^confirm$', views.confirm, name='confirm'),
     url(r'^logout$', views.logout, name='logout'),
+    url(r'^profile$', auth_views.password_change, 
+    	{'template_name': 'profile.html', 'post_change_redirect': '/'},
+    	name='profile'),
 ]
